190d16ad6e638c168ea3235ec49d5e02e89dc959
nam-willow <jnam63213@gmail.com>
2026-04-23 08:31:20 +0900

    update 주석

--- app/services/extraction_service.py
+++ app/services/extraction_service.py
@@ -8,7 +8,11 @@ logger = get_logger(__name__)
 class ExtractionService:
     """
     OCR 결과에서 필요한 필드 추출.
-    나중에 학습된 추출 모델 결과를 여기에서 반환하도록 설계.
+    어떤 문서를, 
+    어떤 필드를, 
+    어떤 정확도로 추출할것인가?
+
+    
     """
 
     def extract_fields(self, ocr_result:  List[Dict[str, Any]]) ->  List[Dict[str, Any]]:
